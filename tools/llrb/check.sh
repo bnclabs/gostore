a824f8caa017771db60d7c78fa3e0f628621e00b
prataprc <prataprc@gmail.com>
2016-03-10 11:19:06 +0530

    tools/llrb: cure check.sh

--- tools/llrb/check.sh
+++ tools/llrb/check.sh
@@ -5,3 +5,4 @@ go build
 # arena {minblock, maxblock, arena-capacity, pool-capacity}
 #llrb check -repeat 100000 -nodearena 96,256,3000000000,1048576 -valarena 96,2048,3000000000,1048576
 llrb check -mvcc 100 -repeat 100000 -nodearena 96,256,3000000000,1048576 -valarena 96,2048,3000000000,1048576
+rm llrb
