44ba4762c6871a98894baeb23075aabfcebff21c
prataprc <prataprc@gmail.com>
2016-05-31 18:10:40 +0530

    remove errors.go

--- errors.go
+++
@@ -1,9 +0,0 @@
-package storage
-
-import "errors"
-
-var ErrorOutofMemory = errors.New("llrb.outofmemory")
-var ErrConfigMissing = errors.New("config.missing")
-var ErrConfigNoString = errors.New("config.nostring")
-var ErrConfigNoNumber = errors.New("config.nonumber")
-var ErrConfigNoBool = errors.New("config.nobool")
