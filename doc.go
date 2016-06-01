cf6a12616e5d9f73bb248e8143a193c84c6fc767
prataprc <prataprc@gmail.com>
2016-06-01 10:38:42 +0530

    storage: godoc intro.

---
+++ doc.go
@@ -0,0 +1,16 @@
+// Package storage implement a collection of storage algorithm.
+//
+// dict:
+//
+// Implement a simple in-memory hash map based on golang map. Primary used
+// as reference for testing.
+//
+// llrb:
+//
+// Implement a version of Left Leaning Red Black tree for sorting and
+// retrieving {key,value}.
+//
+// bubt:
+//
+// Implement a version of Bayer-Tree for sorting and retrieving {key,value}.
+package storage
