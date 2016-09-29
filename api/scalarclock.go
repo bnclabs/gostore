c33bb99467ea9327882e3091bd6c4d368d5dabae
prataprc <prataprc@gmail.com>
2016-09-29 16:41:11 +0530

    api: ScalarClock renamed to Scalarclock.

--- api/scalarclock.go
+++ api/scalarclock.go
@@ -1,47 +1,47 @@
 package api
 
-type ScalarClock uint64
+type Scalarclock uint64
 
 // Update implement Clock{} interface.
-func (this ScalarClock) Update(msg interface{}) Clock {
+func (this Scalarclock) Update(msg interface{}) Clock {
 	if msg != nil {
 		switch val := msg.(type) {
 		case int8:
-			return this + ScalarClock(val)
+			return this + Scalarclock(val)
 		case uint8:
-			return this + ScalarClock(val)
+			return this + Scalarclock(val)
 		case int16:
-			return this + ScalarClock(val)
+			return this + Scalarclock(val)
 		case uint16:
-			return this + ScalarClock(val)
+			return this + Scalarclock(val)
 		case int32:
-			return this + ScalarClock(val)
+			return this + Scalarclock(val)
 		case uint32:
-			return this + ScalarClock(val)
+			return this + Scalarclock(val)
 		case int64:
-			return this + ScalarClock(val)
+			return this + Scalarclock(val)
 		case uint64:
-			return this + ScalarClock(val)
+			return this + Scalarclock(val)
 		case int:
-			return this + ScalarClock(val)
+			return this + Scalarclock(val)
 		case uint:
-			return this + ScalarClock(val)
+			return this + Scalarclock(val)
 		}
 	}
 	return this
 }
 
 // Clone implement Clock{} interface.
-func (this ScalarClock) Clone() Clock {
+func (this Scalarclock) Clone() Clock {
 	return this
 }
 
 // Less implement Clock{} interface.
-func (this ScalarClock) Less(other Clock) bool {
-	return this < other.(ScalarClock)
+func (this Scalarclock) Less(other Clock) bool {
+	return this < other.(Scalarclock)
 }
 
 // LessEqual implement Clock{} interface.
-func (this ScalarClock) LessEqual(other Clock) bool {
-	return this <= other.(ScalarClock)
+func (this Scalarclock) LessEqual(other Clock) bool {
+	return this <= other.(Scalarclock)
 }
--- api/scalarclock_test.go
+++ api/scalarclock_test.go
@@ -3,19 +3,19 @@ package api
 import "testing"
 
 func TestScalarClock(t *testing.T) {
-	clock := ScalarClock(10)
-	refclk := ScalarClock(20)
+	clock := Scalarclock(10)
+	refclk := Scalarclock(20)
 	if rclock := clock.Update(nil); rclock != clock {
 		t.Errorf("expected %v, got %v", clock, rclock)
 	} else if rclock := clock.Update(10); rclock != refclk {
 		t.Errorf("expected %v, got %v", refclk, rclock)
 	} else if rclock := clock.Clone(); rclock != clock {
 		t.Errorf("expected %v, got %v", clock, rclock)
-	} else if ok := clock.Less(ScalarClock(10)); ok == true {
+	} else if ok := clock.Less(Scalarclock(10)); ok == true {
 		t.Errorf("expected %v, got %v", false, true)
-	} else if ok := clock.Less(ScalarClock(9)); ok == true {
+	} else if ok := clock.Less(Scalarclock(9)); ok == true {
 		t.Errorf("expected %v, got %v", true, ok)
-	} else if ok := clock.LessEqual(ScalarClock(10)); ok == false {
+	} else if ok := clock.LessEqual(Scalarclock(10)); ok == false {
 		t.Errorf("expected %v, got %v", true, false)
 	}
 }
