778d02e6e0311c53c849d32c43d9d5a13f532052
prataprc <prataprc@gmail.com>
2017-03-01 16:44:19 +0530

    malloc: simplify initblock() implementation.

--- malloc/debug.go
+++ malloc/debug.go
@@ -7,17 +7,10 @@ import "unsafe"
 
 func initblock(block uintptr, size int64) {
 	var dst []byte
-	initsz := len(poolblkinit)
 	sl := (*reflect.SliceHeader)(unsafe.Pointer(&dst))
-	sl.Data, sl.Len = block, initsz
-	if size > 48 { // TODO: make sure that it is alteast metadata size.
-		size = 48
+	if size > 80 { // TODO: make sure that it is alteast metadata size.
+		size = 80
 	}
-	for i := int64(0); i < size/int64(initsz); i++ {
-		copy(dst, poolblkinit)
-		sl.Data = (uintptr)(uint64(sl.Data) + uint64(initsz))
-	}
-	if sl.Len = int(size) % initsz; sl.Len > 0 {
-		copy(dst, poolblkinit)
-	}
+	sl.Data, sl.Len = block, int(size)
+	copy(dst, poolblkinit)
 }
