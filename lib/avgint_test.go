de166ee474e8ae9bd951e67e1e9a7427134f81ae
prataprc <prataprc@gmail.com>
2017-03-21 11:46:20 +0530

    lib: improvise test cases for AverageInt64{}.

--- lib/avgint_test.go
+++ lib/avgint_test.go
@@ -4,9 +4,20 @@ import "testing"
 
 func TestAverageInt(t *testing.T) {
 	avg := &AverageInt64{}
+
+	if mean := avg.Mean(); mean != 0 {
+		t.Errorf("expected 0, got %v", mean)
+	} else if variance := avg.Variance(); variance != 0 {
+		t.Errorf("expected 0, got %v", variance)
+	} else if sd := avg.SD(); sd != 0 {
+		t.Errorf("expected 0, got %v", sd)
+	}
+
+	// start populating.
 	for i := 1; i <= 100; i++ {
 		avg.Add(int64(i))
 	}
+	// validate
 	if x, y := int64(1), avg.Min(); x != y {
 		t.Errorf("Min() expected %v, got %v", x, y)
 	} else if x, y := int64(100), avg.Max(); x != y {
@@ -22,6 +33,46 @@ func TestAverageInt(t *testing.T) {
 	} else if x, y := int64(29), avg.SD(); x != y {
 		t.Errorf("SD() expected %v, got %v", x, y)
 	}
+	// stats
+	stats := avg.Stats()
+	if x, y := int64(1), stats["min"].(int64); x != y {
+		t.Errorf("Min() expected %v, got %v", x, y)
+	} else if x, y := int64(100), stats["max"].(int64); x != y {
+		t.Errorf("Max() expected %v, got %v", x, y)
+	} else if x, y := int64(100), stats["samples"].(int64); x != y {
+		t.Errorf("Samples() expected %v, got %v", x, y)
+	}
+	x, y := avg.Sum()/stats["samples"].(int64), stats["mean"].(int64)
+	if x != y {
+		t.Errorf("expected %v, got %v", x, y)
+	}
+	if x, y := avg.Sum()/avg.Samples(), avg.Mean(); x != y {
+		t.Errorf("Mean() expected %v, got %v", x, y)
+	} else if x, y := int64(883), stats["variance"].(int64); x != y {
+		t.Errorf("Variance() expected %v, got %v", x, y)
+	} else if x, y := int64(29), stats["stddeviance"].(int64); x != y {
+		t.Errorf("SD() expected %v, got %v", x, y)
+	}
+
+	// clone
+	newavg := avg.Clone()
+	// validate
+	if x, y := int64(1), newavg.Min(); x != y {
+		t.Errorf("Min() expected %v, got %v", x, y)
+	} else if x, y := int64(100), newavg.Max(); x != y {
+		t.Errorf("Max() expected %v, got %v", x, y)
+	} else if x, y := int64(100), newavg.Samples(); x != y {
+		t.Errorf("Samples() expected %v, got %v", x, y)
+	} else if x, y := int64(100*101)/2, newavg.Sum(); x != y {
+		t.Errorf("Sum() expected %v, got %v", x, y)
+	} else if x, y := newavg.Sum()/newavg.Samples(), newavg.Mean(); x != y {
+		t.Errorf("Mean() expected %v, got %v", x, y)
+	} else if x, y := int64(883), newavg.Variance(); x != y {
+		t.Errorf("Variance() expected %v, got %v", x, y)
+	} else if x, y := int64(29), newavg.SD(); x != y {
+		t.Errorf("SD() expected %v, got %v", x, y)
+	}
+
 }
 
 func BenchmarkAvgintAdd(b *testing.B) {
