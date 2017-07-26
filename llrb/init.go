921891e8f6716483cfbd51743d99b3bd4698be96
prataprc <prataprc@gmail.com>
2017-07-26 19:37:15 +0530

    llrb: refactor init.

--- llrb/init.go
+++ llrb/init.go
@@ -1,17 +1,12 @@
 package llrb
 
-import "fmt"
-
 import "github.com/prataprc/golog"
-import "github.com/prataprc/gostore/api"
 import "github.com/prataprc/gostore/malloc"
 import s "github.com/prataprc/gosettings"
 
 // TODO: replace panicerr with panic.
 
 func (llrb *LLRB) readsettings(setts s.Settings) {
-	fmask := llrb.setupfmask(setts)
-	mdsize := int64((&metadata{}).initMetadata(0, fmask).sizeof())
 
 	llrb.iterpoolsize = setts.Int64("iterpool.size")
 	llrb.lsm = setts.Bool("lsm")
@@ -30,39 +25,31 @@ func (llrb *LLRB) readsettings(setts s.Settings) {
 	if llrb.minkeysize < 0 {
 		panicerr("invalid minkeysize settings %v", llrb.minkeysize)
 	}
-	minkeysize := llrb.minkeysize + int64((&Llrbnode{}).sizeof()) + mdsize
+	minkeysize := adjustkeysize(llrb.minkeysize, setts)
 	minkeysize = minkeysize / 32 * 32 // floor minsize
 	fmsg := "%v adjusting minkeysize %v->%v with overhead"
 	log.Infof(fmsg, llrb.logprefix, llrb.minkeysize, minkeysize)
 	llrb.minkeysize = minkeysize
 	// adjust maxkeysize to include overhead
-	maxkeysize := llrb.maxkeysize + int64((&Llrbnode{}).sizeof()) + mdsize
+	maxkeysize := adjustkeysize(llrb.maxkeysize, setts)
 	if (maxkeysize % 32) != 0 { // ceil maxsize
 		maxkeysize = ((maxkeysize / 32) + 1) * 32
 	}
 	fmsg = "%v adjusting maxkeysize %v->%v with overhead"
 	log.Infof(fmsg, llrb.logprefix, llrb.maxkeysize, maxkeysize)
 	llrb.maxkeysize = maxkeysize
-	// validate minkeysize maxkeysize
-	if llrb.minkeysize > api.MaxKeysize {
-		fmsg := "%v minkey %v > %v"
-		panic(fmt.Errorf(fmsg, llrb.logprefix, llrb.minkeysize, api.MaxKeysize))
-	} else if llrb.maxkeysize > api.MaxKeysize {
-		fmsg := "%v maxkey %v > %v"
-		panic(fmt.Errorf(fmsg, llrb.logprefix, llrb.maxkeysize, api.MaxKeysize))
-	}
 
 	// adjust minvalsize to include overhead
 	if llrb.minvalsize < 0 {
 		panicerr("invalid minvalsize.maxblock settings %v", llrb.minvalsize)
 	}
-	minvalsize := llrb.minvalsize + int64((&nodevalue{}).sizeof())
+	minvalsize := adjustvalsize(llrb.minvalsize)
 	minvalsize = minvalsize / 32 * 32 // floor minsize
 	fmsg = "%v adjusting minvalsize %v->%v with overhead"
 	log.Infof(fmsg, llrb.logprefix, llrb.minvalsize, minvalsize)
 	llrb.minvalsize = minvalsize
 	// adjust maxvalsize to include overhead
-	maxvalsize := llrb.maxvalsize + int64((&nodevalue{}).sizeof())
+	maxvalsize := adjustvalsize(llrb.maxvalsize)
 	if (maxvalsize % 32) != 0 { // ceil maxsize
 		maxvalsize = ((maxvalsize / 32) + 1) * 32
 	}
@@ -85,7 +72,7 @@ func (llrb *LLRB) newvaluearena(setts s.Settings) *malloc.Arena {
 	return malloc.NewArena(llrb.valcapacity, memsetts)
 }
 
-func (llrb *LLRB) setupfmask(setts s.Settings) metadataMask {
+func setupfmask(setts s.Settings) metadataMask {
 	fmask := metadataMask(0)
 	if setts.Bool("metadata.bornseqno") {
 		fmask = fmask.enableBornSeqno()
@@ -103,4 +90,14 @@ func (llrb *LLRB) setupfmask(setts s.Settings) metadataMask {
 		fmask = fmask.enableFpos()
 	}
 	return fmask
+}
+
+func adjustkeysize(keysize int64, setts s.Settings) int64 {
+	fmask := setupfmask(setts)
+	mdsize := int64((&metadata{}).initMetadata(0, fmask).sizeof())
+	return keysize + int64((&Llrbnode{}).sizeof()) + mdsize
+}
+
+func adjustvalsize(valsize int64) int64 {
+	return valsize + int64((&nodevalue{}).sizeof())
 }
