cb8662787cbd935b5ee6a89817db39bc1ed0f271
prataprc <prataprc@gmail.com>
2017-10-31 12:38:32 +0530

    bogn: init code for testing.

---
+++ bogn/init_test.go
@@ -0,0 +1,23 @@
+package bogn
+
+import "fmt"
+import "net/http"
+
+import "github.com/prataprc/golog"
+import _ "net/http/pprof"
+
+var _ = fmt.Sprintf("dummy")
+
+func init() {
+	setts := map[string]interface{}{
+		"log.level":      "ignore",
+		"log.colorfatal": "red",
+		"log.colorerror": "hired",
+		"log.colorwarn":  "yellow",
+	}
+	log.SetLogger(nil, setts)
+
+	go func() {
+		log.Infof("%v", http.ListenAndServe("localhost:6060", nil))
+	}()
+}
