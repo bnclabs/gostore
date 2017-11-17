3bae18d70b10119daaff9fc306b92abb4f6a94c0
prataprc <prataprc@gmail.com>
2017-11-17 20:40:59 +0530

    malloc: skip chunk initialization.

--- malloc/production.go
+++ malloc/production.go
@@ -8,8 +8,8 @@ import "unsafe"
 func initblock(block uintptr, size int64) {
 	var dst []byte
 	sl := (*reflect.SliceHeader)(unsafe.Pointer(&dst))
-	if size > 80 { // TODO: make sure that it is alteast metadata size.
-		size = 80
+	if size > 64 { // TODO: make sure that it is alteast metadata size.
+		size = 64
 	}
 	sl.Data, sl.Len = block, int(size)
 	copy(dst, zeroblkinit)
